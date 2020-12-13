e34bd55877ce6267e9c141795b00d311e6557ada
sagarmalam <47811226+sagarmalam@users.noreply.github.com>
2020-12-13 17:08:04 +0530

    Update registrant.c

--- modules/uac_registrant/registrant.c
+++ modules/uac_registrant/registrant.c
@@ -824,7 +824,7 @@ int run_timer_check(void *e_data, void *data, void *r_data)
 	case NOT_REGISTERED_STATE:
 		if(rec->expires==0){
 			if(send_unregister(i, rec, NULL)==1) {
-				rec->state = UNREGISTERING_STATE;
+				rec->state = UNREGISTERED_STATE;
 			} else {
 				rec->state = INTERNAL_ERROR_STATE;
 			}
