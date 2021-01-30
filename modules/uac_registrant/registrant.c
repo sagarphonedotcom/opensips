71cba8ae3fe2668b6d13a446fcae7ca238f55c48
sagarmalam <47811226+sagarmalam@users.noreply.github.com>
2021-01-30 13:29:30 +0530

    Update registrant.c

--- modules/uac_registrant/registrant.c
+++ modules/uac_registrant/registrant.c
@@ -580,8 +580,8 @@ int run_reg_tm_cback(void *e_data, void *data, void *r_data)
 			LM_ERR("failed to build authorization hdr\n");
 			goto done;
 		}
-		rec->auth_hdr.s=new_hdr->s;
-		rec->auth_hdr.len=new_hdr->len;
+		rec->auth_hdr.s=new_hdr.s;
+		rec->auth_hdr.len=new_hdr.len;
 		switch(rec->state) {
 		case REGISTERING_STATE:
 			if(send_register(cb_param->hash_index, rec, new_hdr)==1) {
