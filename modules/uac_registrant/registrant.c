8f223f3942038eb0828c18987599b6f1b53fe4dd
Sagar Malam <sagar.malam@ecosmob.com>
2024-10-02 14:48:47 +0530

    Fix param issue

--- modules/uac_registrant/registrant.c
+++ modules/uac_registrant/registrant.c
@@ -1034,7 +1034,7 @@ int run_timer_check(void *e_data, void *data, void *r_data)
 	case NOT_REGISTERED_STATE:
 		rec->failed_attempts=0;
 		if(rec->expires==0){
-			if(send_unregister(i, rec, NULL)==1) {
+			if(send_unregister(i, rec, NULL,0)==1) {
 				rec->state = UNREGISTERING_STATE;
 			} else {
 				rec->state = INTERNAL_ERROR_STATE;
